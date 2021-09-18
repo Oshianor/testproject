6c7b47e2bc5157065e56368ea3e356bce122c568
oshianor <abundanceoshianor@gmail.com>
2021-09-17 20:33:27 -0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb+srv://abundance:HxznqZ8XQjq2kLXN@churchee.qbavh.gcp.mongodb.net/test')],
 })
 export class AppModule {}
